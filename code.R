4eebecb6de580a905e472730567f6b93eaa52ab3
timelyportfolio <kent.russell@timelyportfolio.com>
2013-11-15 11:36:11 -0600

    first commit, but almost complete
    should have committed earlier to have history but didn't so start here
    almost at the end

---
+++ code.R
@@ -0,0 +1,41 @@
+# use R package scholar to look at Fama and French
+
+# Fama from Google Scholar
+# http://scholar.google.com/citations?user=yP7euFUAAAAJ&hl=en
+# yP7euFUAAAAJ
+
+# French from Google Scholar
+# does not exist
+
+library(scholar)
+famaId = "yP7euFUAAAAJ"
+fama.df <- compare_scholar_careers(famaId)
+#plot(fama.df$cites~fama.df$year, type = "p")
+#abline(lm(fama.df$cites~fama.df$year), col="green")
+
+library(rCharts)
+
+fama.df$date <- paste0(
+  "#! new Date(",
+  as.numeric(as.POSIXct(paste0(fama.df$year,"-12-31"))) * 1000,
+  ")!#"
+)
+
+dy1 <- rCharts$new()
+dy1$setLib( "." )
+dy1$templates$script = "chart.html"
+dy1$set(
+  data = fama.df,
+  x = "date",
+  y = "cites",
+  chart = list(
+    title = "Eugene Fama - Career in Citations from Google Scholar",
+    ylabel = "Citations",
+    showRangeSelector = TRUE,
+    labelsDivStyles= list(
+      background = 'none'
+    ),
+    strokeWidth = 1.5
+  )
+)
+dy1
